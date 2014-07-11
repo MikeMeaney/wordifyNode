8198ae138ba3a23d70474b7b91bd02f30adcaff7
Mike Meaney <mike@calitexaco.com>
2014-07-10 17:05:12 -0700

    Derpity Flerg
    removed shit from code that made shit break

--- app.js
+++ app.js
@@ -4,10 +4,6 @@ var randomWordURL = 'http://api.wordnik.com:80/v4/words.json/randomWord?hasDicti
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
- 
-server.listen(server_port, server_ip_address, function () {
-  console.log( "Listening on " + server_ip_address + ", server_port " + port )
-});
 
 http.createServer(function(req, res) {
     var theWord = "";
